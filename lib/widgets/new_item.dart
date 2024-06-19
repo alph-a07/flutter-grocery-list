92f6d657c52f2308e639cd74088acef62af8ebac
Jeel Patel <itsjeel01@Jeels-MacBook-Pro.local>
2024-06-19 16:23:35 +0530

    Add loading bar while adding item

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -18,12 +18,16 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
+  var _isSending = false;
 
   void _saveItem() async {
     final isValid = _formKey.currentState!.validate();
 
     if (isValid) {
       _formKey.currentState!.save();
+      setState(() {
+        _isSending = true;
+      });
 
       final url = Uri.https(
         'flutter-grocery-list-a6dc6-default-rtdb.firebaseio.com',
@@ -141,12 +145,21 @@ class _NewItemState extends State<NewItem> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   TextButton(
-                      onPressed: () {
-                        _formKey.currentState!.reset();
-                      },
+                      onPressed: _isSending
+                          ? null
+                          : () {
+                              _formKey.currentState!.reset();
+                            },
                       child: const Text('Reset')),
                   ElevatedButton(
-                      onPressed: _saveItem, child: const Text('Add item'))
+                      onPressed: _isSending ? null : _saveItem,
+                      child: _isSending
+                          ? const SizedBox(
+                              height: 16,
+                              width: 16,
+                              child: CircularProgressIndicator(),
+                            )
+                          : const Text('Add item'))
                 ],
               )
             ],
